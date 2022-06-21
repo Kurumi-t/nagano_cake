d46d9969a777c9f77af1479f19d232a88c3fe597
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-06-21 10:57:49 +0000

    [Add]devise用のコントローラ作成ログイン機能のみ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# URL /customers/sign_in ...
+devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: "public/sessions"
+}
+
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+  
 end

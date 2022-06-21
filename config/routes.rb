faa9097a5b539da6b1513bed56af350f60890829
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-06-21 12:12:53 +0000

    ルーティング書き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,68 +1,24 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
+    get "homes/top" => "homes#top", as: "/"
+    resources :orders, only: [:show, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :orders, only: [:show, :update]
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
+  
   namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
+    get 'homes/top' => "homes#top", as: "/" 
+    get 'homes/about' => "homes#about", as: "/about" 
+    resources :orders, only: [:new, :confirm, :thanks, :create, :index, :show]
+    resources :cart_items, only: [:update, :destroy, :destroy_all, :edit]
+    resources :customers, only: [:show, :edit, :update, :withdraw, :withdraw_update]
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update, :withdraw, :withdraw_update]
   end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/thanks'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destoy_all'
-    get 'cart_items/edit'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/withdraw'
-    get 'customers/withdraw_update'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+  
 # URL /customers/sign_in ...
 devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",

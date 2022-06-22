c1ab416f035e50a5532a4121da88d8d80e35b46c
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-06-22 14:18:49 +0000

    管理者ジャンル一覧画面作成、商品とのアソシエーション記述

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,8 +1,12 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.find(params[:id])
   end
 
   def create
+    @genre = genre.new
+    genre.save
+    redirect_to 'index'
   end
 
   def edit

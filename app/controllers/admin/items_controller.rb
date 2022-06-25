7abb474cef801d3b71ab914314ee9cf75236a762
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-06-25 06:01:14 +0000

    商品新規意登録ページのジャンルのセレクトボックス編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,9 +4,12 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @genres = Genre.all
   end
 
   def create
+    genre_id = Genre.find_by(code: params[:item][:genre_id]).id
+    Item.create(item_params.merge(genre_id: genre_id))
   end
 
   def show
@@ -16,5 +19,10 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:item_genre)
   end
 end

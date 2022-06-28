49b0890bf2561c135335c69ade5747773d9f20bb
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-06-28 13:05:33 +0000

    注文履歴詳細ページの編集

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,9 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @customer = Customer.find_by(id: @order.customer_id)
-    @cart_item = Cart_item.find_by(id: @order.)
+    @cart_item = Order.find_by(id: @order.id)
+    @item = Item.find_by(id: @cart_item.item_id)
+    @order_details = Order_detail.find_by(id: @order/)
   end
 
   def update

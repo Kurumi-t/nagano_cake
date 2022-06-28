214cce5cb6c9dcd50f1b54f709f3e79ab3c3ab64
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-06-28 12:25:18 +0000

    [Add]注文履歴詳細ページ、注文履歴一覧ページ作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,8 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @customer = Customer.find_by(id: @order.customer_id)
+    @cart_item = Cart_item.find_by(id: @order.)
   end
 
   def update

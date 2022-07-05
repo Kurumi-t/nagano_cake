4561d9138365bb940b5d616d0322b3c40b452340
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-07-05 15:01:48 +0000

    パス名変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -21,11 +21,11 @@ class Public::SessionsController < Devise::SessionsController
 
   protected
     def after_sign_in_path_for(resource)
-      top_path
+      root_path
     end
 
     def after_sign_out_path_for(resource)
-      top_path
+      root_path
     end
   
   def customer_state

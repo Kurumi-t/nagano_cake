5c67aeca2d8b0e48049a07cccf0cbcb0ab7cf7e6
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-07-06 16:03:35 +0000

    配送先登録／一覧ページ作成、必要のないページの削除、ヘッダーに表示をされる顧客へのメッセージの記述変更

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,28 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @customer = Customer.find_by(id: @address.customer_id)
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    redirect_to addresses_path
   end
 
   def update
+    @address 
+    address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
 end

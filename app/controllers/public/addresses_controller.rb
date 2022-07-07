a2316f82ec79fe31bde9fd1440948c2fdc54f5ac
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-07-07 13:53:10 +0000

    配送先登録／一覧ページの登録、一覧機能作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,22 +1,22 @@
 class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
-    @customer = Customer.find_by(id: @address.customer_id)
     @addresses = Address.all
   end
-
+  
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to addresses_path
+  end
+  
   def edit
     @address = Address.find(params[:id])
   end
 
-  def create
-    address = Address.new(address_params)
-    address.save
-    redirect_to addresses_path
-  end
-
   def update
-    @address 
+    @address = Address.find(params[:id]) 
     address.update(address_params)
     redirect_to addresses_path
   end
@@ -29,6 +29,6 @@ class Public::AddressesController < ApplicationController
   
   private
   def address_params
-    params.require(:address).permit(:postal_code, :address, :name)
+    params.require(:address).permit(:customer_id, :postal_code, :address, :name)
   end
 end

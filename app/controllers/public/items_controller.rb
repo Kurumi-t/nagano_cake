7694eccac79efc0c23570fa66172ff441aad0e27
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-07-08 12:00:33 +0000

    [Add]itemのindexとshowページ作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,10 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
+    @items = Item.page(params[:page])
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end

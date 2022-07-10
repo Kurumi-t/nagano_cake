249329c53d6bfe18c9948a4171f8c2a2a3837ff7
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-07-10 12:34:09 +0000

    小計を求めるメソッドの記述変更

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,12 +2,8 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
   belongs_to :order
-  
-  def post_tax_price
-		item.price * 1.1
-	end
-  
-  def total_price
-		post_tax_price * amount
-	end
+ 
+	def subtotal
+    item.with_tax_price * amount
+  end
 end

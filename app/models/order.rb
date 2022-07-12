94bb8ef9eeeffa6e525bca4f236a22821e744564
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-07-12 10:13:16 +0000

    必要のないアソシエーション記述の削除

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ApplicationRecord
-	has_many :cart_items, dependent: :destroy
 	
 	enum payment_method: { credit_card: 0, transfer: 1 }
 	enum status:  { waiting_payment: 0, payment_confirmation: 1, delivery_preparation: 2, delivered: 3 }

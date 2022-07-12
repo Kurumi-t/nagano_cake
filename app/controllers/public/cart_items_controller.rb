39475b200bacef5234d3240fadbc47cb081ab16a
EC2 Default User <ec2-user@ip-172-31-2-28.ec2.internal>
2022-07-12 13:08:54 +0000

    　ショッピングカートページ小計、単価の３桁区切りの表示設定・update,destoyアクション編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,28 +1,26 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = CartItem.all
-    
     @total_price = 0
   end
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer
+    @cart_item.customer_id = current_customer.id
     @cart_item.save
-    binding.pry
     redirect_to cart_items_path
   end
     
   def update
     @cart_item = CartItem.find(params[:id])
-    @cart_item.update(@cart_item_params)
-    redirect_to cart_items_path(@cart_item.id)
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
-    redirect_to cart_item(@cart_item.id)
+    redirect_to cart_items_path
   end
 
   def destoy_all

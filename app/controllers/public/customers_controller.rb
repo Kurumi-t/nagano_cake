55936bc9eb37242415647bd1d8451d80a12a3516
Kurumi-t <kurumisda@gmail.com>
2022-07-16 05:59:00 +0000

    customer_paramsの記述追記、form_withの記述変更（会員情報の編集ができなかったため）

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -23,7 +23,7 @@ class Public::CustomersController < ApplicationController
     reset_session
     redirect_to top_path
   end
-  
+
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)

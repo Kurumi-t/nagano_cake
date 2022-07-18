de773ad94ac89812f6ddb9b8c476aa345943a31b
Kurumi-t <kurumisda@gmail.com>
2022-07-18 15:37:20 +0000

    変更なし

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items.all
     @total_price = 0
   end
 
@@ -11,10 +11,10 @@ class Public::CartItemsController < ApplicationController
       @cart_item = CartItem.new(cart_item_params)
       @cart_item.customer_id = current_customer.id
     end
-    @cart_item.save
-    redirect_to cart_items_path
+      @cart_item.save
+      redirect_to cart_items_path
   end
-    
+
   def update
     @cart_item = CartItem.find(params[:id])
     @cart_item.update(cart_item_params)
@@ -30,7 +30,7 @@ class Public::CartItemsController < ApplicationController
   def destoy_all
     current_customer.cart_items.destoy_all
   end
-  
+
   private
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)

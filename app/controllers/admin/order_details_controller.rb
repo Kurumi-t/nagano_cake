eb95fcbff0e7a63c20db38f6454a93c460dc9248
Kurumi-t <kurumisda@gmail.com>
2022-07-22 12:06:41 +0000

    update機能作成

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,5 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    
+    @order_detail = OrderDetail.find(params[:id])
+    @order.id = @order_detail.order_id
+    @order_detail.update(order_detail_params)
+    redirect_to admin_order_path(@order.id)
+  end
+
+  private
+  def order_detail_params
+    params.require(:order_detail).permit(:order_id, :item_id, :price, :amount, :making_status)
   end
 end

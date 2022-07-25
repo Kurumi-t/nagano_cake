2da203561c2643436f226b2d47f8b5c7087c6a15
Kurumi-t <kurumisda@gmail.com>
2022-07-25 10:17:54 +0000

    belongs_toにoptionalの記述追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,6 @@
 class CartItem < ApplicationRecord
-  belongs_to :item
-  belongs_to :customer
+  belongs_to :item, optional: true
+  belongs_to :customer, optional: true
 
   def subtotal
     item.with_tax_price * amount

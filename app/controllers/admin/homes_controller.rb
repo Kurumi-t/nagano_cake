0532a102c405f5c478c2f37c7d9c983a4481efe6
Kurumi-t <kurumisda@gmail.com>
2022-07-26 05:12:52 +0000

    必要のない記述の削除

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,8 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
     @orders = Order.page(params[:page])
-    #@order = Order.find(params[:id])
-    #@customer = Customer.find_by(id: @order.customer_id)
-    
   end
 end

9bd9e47de711ccf9af2a14374dcd385c82918e45
Kurumi-t <kurumisda@gmail.com>
2022-07-27 14:32:25 +0000

    order_paramsの記述追記

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,18 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @customer = Customer.find_by(id: @order.customer_id)
-    @cart_item = Order.find_by(id: @order.id)
-    @item = Item.find_by(id: @cart_item.item_id)
-    @order_details = Order_detail.find_by(id: @order)
+    @order_detail = OrderDetail.new
+    @total_price = 0
   end
 
   def update
     order = Order.find(params[:id])
     order.update(order_params)
     redirect_to admin_order_path(order.id)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shopping_cost, :total_payment, :payment_method, :status)
   end
 end

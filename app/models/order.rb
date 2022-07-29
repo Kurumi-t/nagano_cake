07be4afd6d03921943ae597d81fc130564afb7d1
Kurumi-t <kurumisda@gmail.com>
2022-07-29 13:16:21 +0000

    日付を降順に並び変え

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
 	belongs_to :customer, optional: true
 	enum payment_method: { credit_card: 0, transfer: 1 }
 	enum status:  { waiting_payment: 0, payment_confirmation: 1, delivery_preparation: 2, delivered: 3 }
+	default_scope -> { order(created_at: :desc) }
 
 	def total_price
 		@cart_items.to_a.sum { |cart_item| cart_item.total_price }

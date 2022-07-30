9bc6934cd7ed31a38cd5e06c5a0a3b9ea458a8d8
Kurumi-t <kurumisda@gmail.com>
2022-07-30 09:17:59 +0000

    商品件数の機能編集

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,8 @@
 class Public::ItemsController < ApplicationController
   def index
     @genres = Genre.all
-    @items = Item.page(params[:page])
+    @items = Item.page(params[:page]).per(8)
+    @item_count = Item.all.count
   end
 
   def show

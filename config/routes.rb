d0882ec10bc669c1d3e6b49ab8df5d863a76330a
Kurumi-t <kurumisda@gmail.com>
2022-07-30 06:19:24 +0000

    間違った記述の訂正

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root to: 'public/homes#top'
   get '/about' => 'public/homes#about'
-  
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
- 
+
   scope module: :public do
     post '/orders/confirm' => 'orders#confirm'
     get '/orders/thanks' => 'orders#thanks'
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:show, :update]
-    resources :orders_details, only: [:update]
+    resources :order_details, only: [:update]
   end
-  
+
 end

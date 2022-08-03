d3f03ec3d8ee8039812b7f4e552287af01cebcdb
Kurumi-t <kurumisda@gmail.com>
2022-08-03 13:47:27 +0000

    注文ステータスenumの記述追記

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,8 +1,14 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    order_detail = OrderDetail.find(params[:id])
-    order_detail.update(order_detail_params)
-    redirect_to admin_order_path(order_detail.order_id)
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_detail.update(order_detail_params)
+    if @order_detail.making_status == "製作中"
+      @order.update(status: "製作中")
+    elsif @order.order_details.count == @order.order_details.where(making_status: "製作完了").count
+      @order.update(status: "発送準備中")
+    end
+    redirect_to admin_order_path(@order_detail.order_id)
   end
 
   private
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,14 +1,18 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @order_detail = OrderDetail.new
+    @order_details = @order.order_details
     @total_price = 0
   end
 
   def update
-    order = Order.find(params[:id])
-    order.update(order_params)
-    redirect_to admin_order_path(order.id)
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params)
+    if @order.status == "入金確認"
+      @order_details.update_all(making_status: "製作待ち")
+    end
+    redirect_to admin_order_path(@order.id)
   end
 
   private
